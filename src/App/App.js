7c27c248b002ae66cce0c1f302bd27df9ce0afe8
Chris Basham <chrisdbasham0317@gmail.com>
2020-01-10 08:53:47 -0700

    Import form container into app return

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../Header/Header';
+import FormContainer from '../FormContainer/FormContainer';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <FormContainer />
     </div>
   );
 }
