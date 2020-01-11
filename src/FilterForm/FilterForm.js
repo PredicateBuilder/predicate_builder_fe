8247f706525afd73989c348069e0aeadde781094
Chris Basham <chrisdbasham0317@gmail.com>
2020-01-11 16:13:43 -0700

    Add comments to provide insight into thought process and functionality

--- src/FilterForm/FilterForm.js
+++ src/FilterForm/FilterForm.js
@@ -3,23 +3,36 @@ import './FilterForm.scss';
 
 class FilterForm extends Component {
 
+  //This method is designed to send action up to the form container to register a change in either <select> element
   handleDropDownChange = (e) => {
+    //Deconstructs the props to pull out the updateFilter method and the index the the rendered form lives at within the filters array in FormContainer's state
     const { updateFilter, index } = this.props;
+    //Creates an array from the string that is assigned in the targets value
+    //Index 0 is the key to be updated, and index 1 is the updated value
     let valueArr = (e.target.value.split(','));
     updateFilter(valueArr, index);
   };
 
+  //This method is designed to send action up to the form container to register a change in any of the input elements
   handleCustomFieldChange = (e) => {
+    //Deconstruction like above
     const { updateFilter, index } = this.props;
+    //Formats the data so that the updateFilter method can be reused here
+    //...name is the key to be updated, ...value is the new value
     const valueArr = [e.target.name, e.target.value];
     updateFilter(valueArr, index);
   }
 
+  //Method designed to conditionally render the appropriate operator dropdown based on the currently selected predicate
   determineOperatorDropDown = () => {
+    //Deconstruct
     const { operator, predicate } = this.props;
+    //Provides a list of values to check against
     const strings = ['user_email', 'user_first_name', 'user_last_name', 'domain', 'path'];
+    //Creates a variable that stores the current value to be displayed during changes in render
     let operatorValue = ['operator', `${operator}`].join(',');
     if (strings.includes(predicate)) {
+      //this select options renders if the currently selected predicate stores string values
       return (
         <select className='select--operator' onChange={this.handleDropDownChange} value={operatorValue}>
           <option value={['operator', '=']}>equals</option>
@@ -29,6 +42,7 @@ class FilterForm extends Component {
         </select>
       )
     } else {
+      //this select options renders if the currently selected predicate stores integer values
       return (
         <select className='select--operator' onChange={this.handleDropDownChange} value={operatorValue}>
           <option value={['operator', '=']}>equals</option>
@@ -41,27 +55,39 @@ class FilterForm extends Component {
     };
   };
 
+  //This method allows me to conditionally render text/number inputs based up on operator being used
   determineCustomFields = () => {
+    //Deconstruction
     const { operator } = this.props;
     return operator === 'BETWEEN' ?
+      //This return is used when there is a range to be input
       <>
         <input className='range-input' type='number' name='customValue1' onChange={this.handleCustomFieldChange} />
         <p className='p--article'>AND</p>
         <input className='range-input' type='number' name='customValue2' onChange={this.handleCustomFieldChange} />
       </> :
+      //This return is used when only a single input is needed
       <input className='single-input' type={this.determineInputType()} name='customValue1' onChange={this.handleCustomFieldChange} />
   }
 
+  //This method determines which type to assign input based upon the currently selected predicate
   determineInputType = () => {
+    //Deconstruction
     const { predicate } = this.props;
+    //Variable provides a data set to check against
     const strings = ['user_email', 'user_first_name', 'user_last_name', 'domain', 'path'];
+    //Ternary returns based on if the currently selected predicate stores strings or integers
     return strings.includes(predicate) ? 'text' : 'number';
   }
 
   render() {
+    //Deconstruction
     const { operator, predicate, index, handleDeleteForm } = this.props;
+    //Provides a variable to render without calling the method within the return value
     let operatorDropDown = this.determineOperatorDropDown();
+    //Provides a way to store the currently selected predicate in case of re-renders
     let predicateValue = ['predicate', `${predicate}`].join(',');
+        //Provides a variable to render without calling the method within the return value
     let customFields = this.determineCustomFields();
     return (
       <form className='filter-form'>
--- src/FormContainer/FormContainer.js
+++ src/FormContainer/FormContainer.js
@@ -17,28 +17,41 @@ class FormContainer extends Component {
     };
   };
 
+  //This method allows me to update the filters within state at the appropriate index
   updateFilter = (values, i) => {
+    //This variable provides an immutable copy that I can directly manipulate
     const newValues = this.state.filters.slice();
+    //Here I manipulate the copy with the new values
     newValues[i][values[0]] = values[1];
+    //This if statement is used to reassign customValue2 is the operator is changed from a range to anything else
     if (newValues[i].operator !== 'BETWEEN') {
       newValues[i].customValue2 = 'undefined';
     }
+    //Here I set state with the newly modified copy
     this.setState({
       filters: newValues
     });
   };
 
+  //This method allows me to remove a filterForm at the appropriate index
   handleDeleteForm = (e, i) => {
+    //Used to stop hard refresh of the page upon button push
     e.preventDefault();
+    //This was used because I was getting odd behavior that I attributed to the bubbling phase
     e.stopPropagation();
+    //This provides a copy of the current state without the form that is being deleted
     const updatedFilters = this.state.filters.filter((filter, index) => index !== i);
+    //This updates state with the mutated array
     this.setState({
       filters: updatedFilters
     });
   };
 
+  //This method handles rendering an additional FilterForm below the last one
   handleAddForm = (e) => {
+    //Used to stop hard refresh of the page upon button push
     e.preventDefault();
+    //This spreads in the previous state along with a new set of default values added to the end
     this.setState({
       filters: [...this.state.filters, {
         predicate: 'user_email',
@@ -49,19 +62,27 @@ class FormContainer extends Component {
     });
   };
 
+  //This method sends the query to my endpoint
   sendQuery = async () => {
+    //This stringifies the filters within state so that they may be passed via query string
     const options = JSON.stringify(this.state.filters);
+    //This sends the request with the stringified state interpolated onto the end
     const res = await fetch(`http://localhost:3001/?filters=${options}`);
+    //This parses the response into readable format
     const builtQuery = await res.json();
+    //This sets the response to state
     this.setState({
       response: builtQuery
     })
   }
 
   render() {
+    //This provides a variable to render that maps through the filters within my state and renders each
     const mapForms = this.state.filters.map((form, i) => {
       return (<FilterForm
+        //Passing props
         key={i}
+        //How I track which form to manipulate
         index={i}
         updateFilter={this.updateFilter}
         handleDeleteForm={this.handleDeleteForm}
@@ -74,8 +95,10 @@ class FormContainer extends Component {
     return (
       <main className='main'>
         <section className='filter-section'>
+          {/* renders each form that is returned by mapForms*/}
           {mapForms}
           <button className='add-filter-form' onClick={this.handleAddForm}>AND</button>
+        {/* conditionally renders a response if it exists in state */}
         {this.state.response && <p>{this.state.response}</p>}
         </section>
         <button className='search-btn' onClick={this.sendQuery}>Search</button>
