4e6a9d7cfc60f6c21f15585b2695adc1a788e53c
Chris Basham <chrisdbasham0317@gmail.com>
2020-01-10 21:47:53 -0700

    Add "and" btn and "-" btn functionality

--- src/FilterForm/FilterForm.js
+++ src/FilterForm/FilterForm.js
@@ -1,57 +1,61 @@
 import React, { Component } from 'react';
 import './FilterForm.scss';
-import { Component } from 'react';
 
 class FilterForm extends Component {
-  constructor() {
-    super()
-    this.predicate = '';
-    this.operator = '';
-    this.customValue = '';
-  }
+
+  handleChange = (e) => {
+    const { updateFilter, index } = this.props;
+    let valueArr = (e.target.value.split(','));
+    updateFilter(valueArr, index);
+  };
 
   determineOperatorDropDown = () => {
     const strings = ['user_email', 'user_first_name', 'user_last_name', 'domain', 'path'];
-    const integers = ['screen_width', 'screen_height', 'visits', 'page_response'];
-    if (strings.includes(this.predicate)) {
+    let operatorValue = ['operator', `${this.props.operator}`].join(',')
+
+    if (strings.includes(this.props.predicate)) {
       return (
-        <select>
-          <option value='EQUAL'>equals</option>
-          <option value='CONTAINS'>contains</option>
-          <option value='LIKE'>starts with</option>
-          <option value='IN'>in list</option>
+        <select onChange={this.handleChange} value={operatorValue}>
+          <option value={['operator', '=']}>equals</option>
+          <option value={['operator','CONTAINS']}>contains</option>
+          <option value={['operator','LIKE']}>starts with</option>
+          <option value={['operator','IN']}>in list</option>
         </select>
       )
     } else {
       return (
-        <select>
-          <option value='='>equals</option>
-          <option value='BETWEEN'>range</option>
-          <option value='>'>greater than</option>
-          <option value='<'>less than</option>
-          <option value='IN'>in list</option>
+        <select onChange={this.handleChange} value={operatorValue}>
+          <option value={['operator', '=']}>equals</option>
+          <option value={['operator', 'BETWEEN']}>range</option>
+          <option value={['operator', '>']}>greater than</option>
+          <option value={['operator', '<']}>less than</option>
+          <option value={['operator', 'IN']}>in list</option>
         </select>
       )
     }
   }
 
   render() {
-    <form>
-      <button>-</button>
-      <select>
-        <option value='user_email'>User Email</option>
-        <option value='screen_width'>Screen Width</option>
-        <option value='screen_height'>Screen Height</option>
-        <option value='visits'># of Visits</option>
-        <option value='user_first_name'>First Name</option>
-        <option value='user_last_name'>Last Name</option>
-        <option value='page_response'>Page Response Time (ms)</option>
-        <option value='domain'>Domain</option>
-        <option value='path'>Page Path</option>
-      </select>
-      {this.determineOperatorDropDown()}
-    </form>
-  }
-}
+    let operator = this.determineOperatorDropDown();
+    let predicateValue = ['predicate', `${this.props.predicate}`].join(',') 
+    return (
+      <form>
+        <button onClick={(e) => this.props.handleDeleteForm(e, this.props.index)}>-</button>
+        <select onChange={this.handleChange} value={predicateValue}>
+          <option value={['predicate', 'user_email']}>User Email</option>
+          <option value={['predicate', 'screen_width']}>Screen Width</option>
+          <option value={['predicate', 'screen_height']}>Screen Height</option>
+          <option value={['predicate', 'visits']}># of Visits</option>
+          <option value={['predicate', 'user_first_name']}>First Name</option>
+          <option value={['predicate', 'user_last_name']}>Last Name</option>
+          <option value={['predicate', 'page_response']}>Page Response Time (ms)</option>
+          <option value={['predicate', 'domain']}>Domain</option>
+          <option value={['predicate', 'path']}>Page Path</option>
+        </select>
+        {operator}
+      </form>
+    );
+  };
+};
 
 export default FilterForm;
--- src/FormContainer/FormContainer.js
+++ src/FormContainer/FormContainer.js
@@ -6,13 +6,61 @@ import './FormContainer.scss';
 class FormContainer extends Component {
   constructor() {
     super()
-    this.filters = []
+    this.state = {
+      filters: [{predicate: 'user_email', operator: '=', customValue: ''}]
+    }
   }
 
+  updateFilter = (values, i) => {
+    const newValues = this.state.filters.slice()
+    newValues[i][values[0]] = values[1];
+    this.setState({filters:newValues})
+  }
+
+  handleDeleteForm = (e, i) => {
+    e.preventDefault();
+    e.stopPropagation();
+    const updatedFilters = this.state.filters.filter((filter, index) => index !== i);
+    this.setState({
+      filters: updatedFilters
+    })
+  }
+
+  handleAddForm = (e) => {
+    e.preventDefault();
+    this.setState({
+      filters: [...this.state.filters, {
+        predicate: 'user_email',
+        operator: '=',
+        customValue: ''
+      }]
+    })
+  }
+
+  
+
   render() {
-    <main>
-      <FilterForm />
-    </main>
+    const mapForms = this.state.filters.map((form, i) => {
+      return (<FilterForm
+        key={i}
+        index={i}
+        updateFilter={this.updateFilter}
+        handleDeleteForm={this.handleDeleteForm}
+        predicate={form.predicate}
+        operator={form.operator}
+        customValue={form.customValue}
+        display={form.display}
+      />);
+    });
+    return (
+      <main>
+        <section className='filter-section'>
+          {mapForms}
+          <button className='add-filter-form' onClick={this.handleAddForm}>AND</button>
+        </section>
+        <button className='search-btn'>SEARCH</button>
+      </main>
+    )  
   }
 }
 
